a2af0013b0bb2eec7e14c40c2aa2c3a3045d2ef8
Mengye Ren <renmengye@gmail.com>
2017-05-25 15:01:09 -0400

    update slurm config

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+NAME=${DIR##*/}
 mkdir -p data
 ln -s /ais/gobi4/mren/data/cifar-10 data/cifar-10
 ln -s /ais/gobi4/mren/data/cifar-100 data/cifar-100
 ln -s /ais/gobi4/mren/data/imagenet data/imagenet
 mkdir -p logs/default
 ln -s /u/$USER/public_html/results logs/public
-mkdir -p /ais/gobi5/$USER/results/resnet
-ln -s /ais/gobi5/$USER/results/resnet results
+mkdir -p /ais/gobi5/$USER/results/$NAME
+ln -s /ais/gobi5/$USER/results/$NAME results
--- sync_remote.sh
+++ sync_remote.sh
@@ -1,9 +1,11 @@
 FOLDER=code
-NAME=resnet
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+NAME=${DIR##*/}
 rsync -rP \
 --exclude "/$NAME/data" \
 --exclude "/$NAME/results" \
 --exclude "/$NAME/logs" \
+--exclude "/$NAME/tmp" \
 --exclude ".git*" \
 --exclude "*.pyc" \
 ../$NAME $USER@cs.toronto.edu:/u/$USER/$FOLDER 
