f4508f2ca6fb96276f4a9b4cdc45505756e88087
Mengye Ren <renmengye@gmail.com>
2017-05-31 15:55:12 -0400

    update

--- README.md
+++ README.md
@@ -35,10 +35,15 @@ bazel build //resnet/experiments:all
 SSH into the slurm manager node first, and then launch jobs there.
 ```
 # Launch a recurring training job, 30K steps per job, for total 600K steps.
-./run_imagenet_exp_sched.py --model resnet-50 --max_num_steps 30000 --max_max_steps 600000
+./bazel-bin/resnet/experiments/run_imagenet_exp_sched \
+  --model resnet-50 \
+  --max_num_steps 30000 \
+  --max_max_steps 600000
 
 # Launch a recurring evaluation job every 2 hours.
-./run_imagenet_eval_sched.py --id [EXPERIMENT ID] --min_interval 7200
+./bazel-bin/resnet/experiments/run_imagenet_eval_sched \
+  --id [EXPERIMENT ID] \
+  --min_interval 7200
 ```
 
 ## Provided Model Configs
--- resnet/configs/test_configs.py
+++ resnet/configs/test_configs.py
@@ -2,8 +2,7 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
 from resnet.configs.config_factory import RegisterConfig
-from resnet.configs.cifar_configs import (ResNet32Cifar10Config,
-                                          RevResNet38Cifar10Config)
+from resnet.configs.cifar_configs import ResNet32Cifar10Config
 
 
 class ResNetTestConfig(ResNet32Cifar10Config):
@@ -20,60 +19,13 @@ def get_resnet_test():
   return ResNetTestConfig()
 
 
-class RevResNetTestConfig(RevResNet38Cifar10Config):
+class ResNetBottleneckTestConfig(ResNetTestConfig):
 
   def __init__(self):
-    super(RevResNetTestConfig, self).__init__()
-    self.batch_size = 10
-    self.num_residual_units = [2, 2]
-    self.filters = [16, 16, 32]
-    self.height = 8
-    self.width = 8
-    self.model_class = "revresnet"
+    super(ResNetBottleneckTestConfig, self).__init__()
+    self.use_bottlneck = True
 
 
-@RegisterConfig("test", "revresnet-test")
-def get_revresnet_test():
-  return RevResNetTestConfig()
-
-
-class RevResNetBottleneckTestConfig(RevResNet38Cifar10Config):
-
-  def __init__(self):
-    super(RevResNetBottleneckTestConfig, self).__init__()
-    self.batch_size = 10
-    self.num_residual_units = [2, 2]
-    self.filters = [16, 16, 32]
-    self.height = 8
-    self.width = 8
-    self.model_class = "revresnet"
-    self.use_bottleneck = True
-
-
-@RegisterConfig("test", "revresnet-btl-test")
-def get_revresnet_btl_test():
-  return RevResNetBottleneckTestConfig()
-
-
-class RevResNetV2TestConfig(RevResNetTestConfig):
-
-  def __init__(self):
-    super(RevResNetV2TestConfig, self).__init__()
-    self.model_class = "revresnet-v2"
-
-
-@RegisterConfig("test", "revresnet-v2-test")
-def get_revresnet_v2_test():
-  return RevResNetV2TestConfig()
-
-
-class RevResNetBottleneckV2TestConfig(RevResNetBottleneckTestConfig):
-
-  def __init__(self):
-    super(RevResNetBottleneckV2TestConfig, self).__init__()
-    self.model_class = "revresnet-v2"
-
-
-@RegisterConfig("test", "revresnet-btl-v2-test")
-def get_revresnet_btl_v2_test():
-  return RevResNetBottleneckV2TestConfig()
+@RegisterConfig("test", "resnet-btl-test")
+def get_resnet_test():
+  return ResNetTestConfig()
